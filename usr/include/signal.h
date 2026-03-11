468758c037b017e4e23c1d4057506ea94e9bf261
James B. Card <jbcard@skymond-software.com>
2026-03-11 01:52:56 -0700

    Adding usr/include/signal.h.

---
+++ usr/include/signal.h
@@ -0,0 +1,56 @@
+///////////////////////////////////////////////////////////////////////////////
+///
+/// @author            Brian Card
+/// @date              03.09.2026
+///
+/// @file              signal.h
+///
+/// @brief             Functionality in the Unix signal library.
+///
+/// @copyright
+///                      Copyright (c) 2026 Brian Card
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included
+/// in all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+///                                Brian Card
+///                      https://github.com/brian-card
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef SIGNAL_H
+#define SIGNAL_H
+
+#include "NanoOsUser.h"
+#include "../../src/user/NanoOsSignal.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+static inline int kill(pid_t pid, int sig) {
+  return overlayMap.header.osApi->kill(pid, sig);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SIGNAL_H
+
