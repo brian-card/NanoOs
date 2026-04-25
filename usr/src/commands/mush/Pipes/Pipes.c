59937656b678189457e3984c81f850bc11a2b0ac
Brian Card <jbcard@gmail.com>
2026-04-25 06:11:11 -0700

    Improving error messages in processPipes of usr/src/commands/mush/Pipes/Pipes.c.

--- usr/src/commands/mush/Pipes/Pipes.c
+++ usr/src/commands/mush/Pipes/Pipes.c
@@ -152,6 +152,7 @@ void* processPipes(void *args) {
       NULL, "FilesystemCommands", "runFsCommand", fsCommandArgs));
     if (pids[numPipes] < 0) {
       // errno is already set
+      fprintf(stderr, "Launching \"%s\" failed\n", fsCommandArgs->commandLine);
       posix_spawn_file_actions_destroy(fileActions);
       goto freeFileActions;
     }
@@ -191,6 +192,7 @@ void* processPipes(void *args) {
   fsCommandArgs->fileActions = NULL;
   callOverlayFunction(
     NULL, "FilesystemCommands", "runFsCommand", fsCommandArgs);
+  fprintf(stderr, "Launching \"%s\" failed\n", fsCommandArgs->commandLine);
   
   // If we made it this far then errno is already set
   
