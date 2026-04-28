2fb696c099ff66d4c59999dea4f5bdb84e245b89
Brian Card <jbcard@gmail.com>
2026-04-28 06:05:06 -0700

    Changing halAdafruitFeatherM0WifiInit in HalAdafruitFeatherM0Wifi.cpp to use halCommonInit.

--- src/hal/HalAdafruitFeatherM0Wifi.cpp
+++ src/hal/HalAdafruitFeatherM0Wifi.cpp
@@ -1093,55 +1093,11 @@ const Hal* halAdafruitFeatherM0WifiInit(void) {
   
   __enable_irq();  // Ensure global interrupts are enabled
   
-  int ii = 0;
-  Hal *hal = &adafruitFeatherM0WifiHal;
-  if (hal->uartHal != NULL) {
-    int numUarts = hal->uartHal->getNumUarts();
-    if (numUarts <= 0) {
-      // Nothing we can do.
-      return NULL;
-    }
-    
-    // Set all the serial ports to run at 1000000 baud.
-    if (hal->uartHal->initUart(0, 1000000) < 0) {
-      // Nothing we can do.
-      return NULL;
-    }
-    for (ii = 1; ii < numUarts; ii++) {
-      if (hal->uartHal->initUart(ii, 1000000) < 0) {
-        // We can't support more than the last serial port that was successfully
-        // initialized.
-        break;
-      }
-    }
-    hal->uartHal->setNumUarts(ii);
-    if (ii != numUarts) {
-      Serial.begin(1000000);
-      while (!Serial);
-      Serial.print("WARNING: Only initialized ");
-      Serial.print(ii);
-      Serial.print(" serial ports\n");
-    }
+  if (halCommonInit(&adafruitFeatherM0WifiHal) != 0) {
+    return NULL;
   }
 
-  if (hal->timerHal != NULL)  {
-    int numTimers = hal->timerHal->getNumTimers();
-    for (ii = 0; ii < numTimers; ii++) {
-      if (hal->timerHal->initTimer(ii) < 0) {
-        break;
-      }
-    }
-    hal->timerHal->setNumTimers(ii);
-    if (ii != numTimers) {
-      Serial.begin(1000000);
-      while (!Serial);
-      Serial.print("WARNING: Only initialized ");
-      Serial.print(ii);
-      Serial.print(" timers\n");
-    }
-  }
-  
-  return hal;
+  return &adafruitFeatherM0WifiHal;
 }
 
 #endif // ADAFRUIT_FEATHER_M0
