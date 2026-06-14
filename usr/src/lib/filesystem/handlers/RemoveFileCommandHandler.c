284901178f1ba1d99bb554c2d7a8d691eddc384e
Brian Card <jbcard@gmail.com>
2026-06-13 17:44:41 -0700

    Adding usr/src/lib/filesystem/handlers/RemoveFileCommandHandler.c.

---
+++ usr/src/lib/filesystem/handlers/RemoveFileCommandHandler.c
@@ -0,0 +1,69 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//                       Copyright (c) 2026 Brian Card
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//                                 Brian Card
+//                       https://github.com/brian-card
+//
+////////////////////////////////////////////////////////////////////////////////
+
+/// @file RemoveFileCommandHandler.c
+///
+/// @brief Overlay implementation of a remove command handler.
+
+// Standard C includes
+#include <stdlib.h>
+#include <string.h>
+
+// NanoOs includes
+#include "KernelProcesses.h"
+#include "NanoOsUtils.h"
+#include "../../filesystem/include/OverlayFilesystem.h"
+
+// Prototypes used by this overlay.
+int driverRemove(void *driverState, const char *pathname);
+
+/// @fn void* RemoveFile(void *args)
+///
+/// @brief Overlay implementation of a remove command handler.
+///
+/// @param args A pointer to a FilesystemState, cast to a void*.  The args
+///   member variable is a pointer to a ProcessMessage.
+///
+/// @return Sets the remove member of the provided FilesystemRemoveArgs to the
+/// value that is to be used for the process's errno value.  This function
+/// always returns the filesystemState pointer provided as args.
+void* RemoveFile(void *args) {
+  FilesystemState *filesystemState = (FilesystemState*) args;
+  ProcessMessage *processMessage = (ProcessMessage*) filesystemState->args;
+  FilesystemRemoveArgs *filesystemRemoveArgs
+    = (FilesystemRemoveArgs*) processMessageData(processMessage);
+  int returnValue = 0;
+  if (filesystemState->driverState != NULL) {
+    returnValue = driverRemove(
+      filesystemState->driverState, filesystemRemoveArgs->pathname);
+  }
+
+  filesystemRemoveArgs->returnValue = returnValue;
+  processMessageSetDone(processMessage);
+  return filesystemState;
+}
+
