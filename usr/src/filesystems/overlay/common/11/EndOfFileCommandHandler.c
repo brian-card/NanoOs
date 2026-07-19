138e75a115dde8a078789092af4c9cf22d7e0383
Brian Card <jbcard@gmail.com>
2026-07-19 12:54:55 -0700

    Initial commit of usr/src/filesystems/overlay/common/11/EndOfFileCommandHandler.c.

---
+++ usr/src/filesystems/overlay/common/11/EndOfFileCommandHandler.c
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//                       Copyright (c) 2026 Brian Card
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//                                 Brian Card
+//                       https://github.com/brian-card
+//
+////////////////////////////////////////////////////////////////////////////////
+
+/// @file EndOfFileCommandHandler
+///
+/// @brief Overlay implementation of a FILESYSTEM_END_OF_FILE command handler.
+
+// Standard C includes
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+// NanoOs includes
+#include "KernelProcesses.h"
+#include "NanoOsUtils.h"
+#include "OverlayFilesystem.h"
+
+// Prototypes used by this overlay.
+int driverFeof(void *fileHandle);
+
+/// @fn void* EndOfFile(void *args)
+///
+/// @brief Overlay implementation of an feof function.
+///
+/// @param args A pointer to a FilesystemState, cast to a void*.  The args
+///   member variable is a pointer to a ProcessMessage.
+///
+/// @return Sets the metadata value of the provided GetFileBlockMetadataArgs to
+//  the value that is to be used by the calling process.  This function always
+/// returns the filesystemState pointer provided as args.
+void* EndOfFile(void *args) {
+  FilesystemState *filesystemState = (FilesystemState*) args;
+  ProcessMessage *processMessage = (ProcessMessage*) filesystemState->args;
+  FeofArgs *args = (FeofArgs*) processMessageData(processMessage);
+  args->returnValue = 0;
+
+  if (filesystemState->driverState != NULL) {
+    args->returnValue = driverFeof(args->stream->file);
+  }
+
+  processMessageSetDone(processMessage);
+  return filesystemState;
+}
+
