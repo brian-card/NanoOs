caf84af3033d6bf0e448f2bad5e747ecd204666f
Brian Card <jbcard@gmail.com>
2026-08-14 15:32:56 -0700

    Initial commit of CoroutineStdio.h so that the Coroutines library can use stdio functions from NanoOs.

---
+++ src/kernel/CoroutineStdio.h
@@ -0,0 +1,63 @@
+///////////////////////////////////////////////////////////////////////////////
+///
+/// @author            Brian Card
+/// @date              08.14.2026
+///
+/// @file              CoroutineStdio.h
+///
+/// @brief             Definitions for using stdio calls from the Coroutines.c
+///                    library within NanoOs.
+///
+/// @copyright
+///                      Copyright (c) 2026 Brian Card
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included
+/// in all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+///                                Brian Card
+///                      https://github.com/brian-card
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef COROUTINE_STDIO_H
+#define COROUTINE_STDIO_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct NanoOsFile NanoOsFile;
+int nanoOsFprintf(NanoOsFile *stream, const char *format, ...);
+#ifdef fprintf
+#undef fprintf
+#endif
+#define fprintf nanoOsFprintf
+
+#ifdef stderr
+#undef stderr
+#endif // stderr
+#define stderr nanoOsStderr
+extern NanoOsFile *nanoOsStderr;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // COROUTINE_STDIO_H
+
