dccb5e88ce5e62238d9cb9a244a9792c3d7f2275
Brian Card <jbcard@gmail.com>
2026-08-23 17:42:02 -0700

    Finishing first-pass implementation of src/hal/AgonLight2/Spi.asm.

--- src/hal/AgonLight2/Spi.asm
+++ src/hal/AgonLight2/Spi.asm
@@ -36,8 +36,7 @@
 .text
 
 .global _agonLight2ConfigureSpiImpl
-.global _agonLight2PollSpiImpl
-.global _agonLight2WriteSpiImpl
+.global _agonLight2SpiTransfer8Impl
 
 ;; -- eZ80F92 Port B GPIO registers ------------------
 PB_DR       .equ 0x9A      ; Port B data register
@@ -53,7 +52,7 @@ SPI_SR,     .equ 0xBB
 SPI_TSR,    .equ 0xBC
 SPI_RBR,    .equ 0xBC
 
-;; -- void agonLight2ConfigureSpiImpl(uint16_t divisor) ---------------
+;; -- void agonLight2ConfigureSpiImpl(uint16_t divisor) -------------
 ;;    divisor passed at sp+3 (low byte), sp+4 (high byte)
 _agonLight2ConfigureSpiImpl:
     push    ix
@@ -61,60 +60,46 @@ _agonLight2ConfigureSpiImpl:
     add     ix, sp
 
     ;; Set PB7, PB6, PB3, and PB2 to mode 7 (alternate function)
-    in0   a, (PB_ALT1)
-    or    a, 0xCC
-    out0  (PB_ALT1), a
+    in0     a, (PB_ALT1)
+    or      a, 0xCC
+    out0    (PB_ALT1), a
 
-    in0   a, (PB_ALT2)
-    or    a, 0xCC
-    out0  (PB_ALT2), a
+    in0     a, (PB_ALT2)
+    or      a, 0xCC
+    out0    (PB_ALT2), a
 
     ;; Set the speed of the SPI bus.  The caller is responsible for passing the
     ;; desired divisor into this function, where:
     ;;
     ;; divisor = system clock / (2 * SPI clock)
-    ld    a, (ix+6)
-    out0  (SPI_BRG_L), a
-    ld    a, (ix+7)
-    out0  (SPI_BRG_H), a
+    ld      a, (ix+6)
+    out0    (SPI_BRG_L), a
+    ld      a, (ix+7)
+    out0    (SPI_BRG_H), a
 
     ;; Set SPIEN | MASTEREN, CPOL=0 CPHA=0
-    ld    a, 0x30
-    out0  (SPI_CTL), a
+    ld      a, 0x30
+    out0    (SPI_CTL), a
 
     pop     ix
     ret
 
-
-;; -- int agonLight2PollSpiImpl(void) ----------------------------
-;;    returns received byte in HL, or -1 if no data ready (non-blocking)
-_agonLight2PollSpiImpl:
-    in0     a, (UART0_LSR)
-    and     0x01                ; DR — data ready
-    jr      z, .Lno_char
-
-    in0     a, (UART0_RBR)
-    ld      hl, 0
-    ld      l, a
-    ret
-
-.Lno_char:
-    ld      hl, -1
-    ret
-;; -- void agonLight2WriteSpiImpl(uint8_t c) ----------------------
-;;    c passed at ix+6
-_agonLight2WriteSpiImpl:
+;; -- int agonLight2SpiTransfer8Impl(uint8_t c) ---------------------
+;;    c passed at sp+3
+_agonLight2SpiTransfer8Impl:
     push    ix
     ld      ix, 0
     add     ix, sp
 
-.Ltx_wait:
-    in0     a, (UART0_LSR)
-    and     0x20                ; THRE — transmit holding register empty
-    jr      z, .Ltx_wait
-
     ld      a, (ix+6)
-    out0    (UART0_THR), a
+    out0    (SPI_TSR), a
 
-    pop     ix
+.spiTransferWait:
+    in0     a, (SPI_SR)
+    bit     7, a
+    jr      z, .spiTransferWait
+
+    in0     a, (SPI_RBR)
+    ld      hl, 0
+    ld      l, a
     ret
